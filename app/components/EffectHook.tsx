48fe177c3fca5a05370015a272f3e3beb2f0fefd
Kevin Stout <stoutkev3@gmail.com>
2023-09-06 18:19:01 -0400

    adding some better definitions to the useEffect file

--- app/components/EffectHook.tsx
+++ app/components/EffectHook.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { clear } from "console";
+
+// useEffect is a React Hook that lets you synchronize a component with an external system.
+// Some components need to stay connected to the network, some browser API, or a third-party library, while they are displayed on the page. These systems aren’t controlled by React, so they are called external.
+
 // The useEffect Hook allows you to perform side effects in your function components.
 // Some examples of side effects are: fetching data, directly updating the DOM, and timers.
-// useEffect accepts two arguments. The second argument is optional. But it is recommended to include it to avoid bugs.
+// useEffect accepts two arguments. The second argument is optional. But it is recommended to include it to avoid bugs. not including it can cause the useEffect function to run on every render.
 // passing in an empty array as the second argument will only run the useEffect function once.
 // useEffect(<function>, <dependency>)
 
