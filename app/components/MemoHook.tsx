9f76260d37b131d0867aab2f24ee30abbb0901cc
Kevin Stout <stoutkev3@gmail.com>
2023-09-10 14:30:12 -0400

    adding some notes to the useMemo hook file

---
+++ app/components/MemoHook.tsx
@@ -0,0 +1,6 @@
+// The React useMemo hook returns a memoized value.
+
+// Think of memoization as chaching a value so that it does not need to be recalculated.
+// The useMemo hook only runs when one of its dependencies update.
+// This can improve performance by reducing the number of times a function is called.
+// The useMemo and useCallback hooks are similar. The difference is that useMemo returns a memoized value and useCallback returns a memoized function.
